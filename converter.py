7e4c62be7df247453e3e3e95f0aeab671d4c6d21
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2021-08-26 14:51:55 +0530

    read

--- converter.py
+++ converter.py
@@ -13,42 +13,42 @@ a = input('\nEnter the function number: ')
 a = float(a)
 
 if a == 1:
-    b = input('enter the minute: ')
+    b = input('Enter the minute: ')
     b = float(b)
 
     b = b * 60
-    print('Here your minutes are converted into second: ' + str(b) + str('second'))
+    print('\nHere your minutes are converted into second: ' + str(b) + str('second'))
 if a == 2:
-    b = input('enter the seconds: ')
+    b = input('Enter the seconds: ')
     b = float(b)
 
     b = b / 60
-    print('Here your seconds are converted into minute: ' + str(b)+str(' min'))
+    print('\nHere your seconds are converted into minute: ' + str(b)+str(' min'))
 
 elif a == 3:
     c = input('Enter the kilometre: ')
     c = float(c)
 
     k = c*1000
-    print('Here your kilometre are converted into metre: ' + str(k)+str(' metre'))
+    print('\nHere your kilometre are converted into metre: ' + str(k)+str(' metre'))
 
 elif a == 4:
     c = input('Enter the metre: ')
     c = float(c)
 
     k = c / 1000
-    print('Here your metre are converted into kilometre: ' + str(k) + str(' kilometre'))
+    print('\nHere your metre are converted into kilometre: ' + str(k) + str(' kilometre'))
 elif a == 5:
-    d = input('enter the feets: ')
+    d = input('Enter the feets: ')
     d = float(d)
 
     h = d*12
-    print('Here your feets are converted into inches: ' + str(h) + str('inches'))
+    print('\nHere your feets are converted into inches: ' + str(h) + str('inches'))
 
 elif a == 6:
-    d = input('enter the inches: ')
+    d = input('Enter the inches: ')
     d = float(d)
 
     h = d / 12
-    print('Here your inches are converted into feets: ' + str(h) + str('feets'))
+    print('\nHere your inches are converted into feets: ' + str(h) + str('feets'))
 
---
+++ readme.md
@@ -0,0 +1,68 @@
+#Converter
+<hr>
+
+This program is made by me to make easy to you in subjects like science-1 and maths, by solving the problems in science-1 and maths you may have arise difficulties to convert Minutes into second , second into minute or Kilometre into metre , metre into kilometre or feets into inches , inches into feets
+
+Don't worry this program will fix your problem
+
+![Output](outputs/p.png)
+
+###Minute into seconds
+
+while converting into minute into seconds
+
+```multiply the time value by 60```
+
+program output:
+
+![Output](outputs/ms.png)
+
+###Seconds into minute
+while converting into seconds into minute
+
+```divide the time value by 60```
+
+program output:
+
+![Output](outputs/sm.png)
+
+###Kilometre into metre
+while converting into kilometre into metre
+
+```multiply the length value by 1000```
+
+program output:
+
+![Output](outputs/km.png)
+
+###Metre into kilometre
+
+while converting metre into kilometre
+
+```divide the length value by 1000```
+
+program output:
+
+![Output](outputs/mk.png)
+
+### Feet into inches
+while converting into feets into inches
+
+```multiply the length value by 12```
+
+program output:
+
+![Output](outputs/fi.png)
+
+###Inches into feets
+while converting into inches into feets
+
+```divide the length value by 12```
+
+![Output](outputs/if.png)
+
+
+If you want more function in this program 
+
+email me : tanmayrmohite@gmail.com
+
